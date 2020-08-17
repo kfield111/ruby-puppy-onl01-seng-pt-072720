33b885cac7d03e0671e624670c1e07376561e73a
Kelin Field <kfield111@hotmail.com>
2020-08-17 23:41:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Add your code here
+class Dog
+  @@all = []
+
+  def initialize
+    @@all.push self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each do |name|
+      puts "#{name}"
+    end
+  end
+  
+
+end
